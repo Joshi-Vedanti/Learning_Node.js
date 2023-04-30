17ea8717e541ab1bcc292cbe41a64f57c6b6e3d8
Sumit Yadav <sumityadav0376@gmail.com>
2023-04-30 16:55:13 +0530

    sent first response

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express();
 
 const port = 8000;
 
+app.get('/', function(req, res){
+    console.log("Request accepted", req.url);
+    
+    return res.send('<h6>This is some text</h6>');
+
+    // return res.status(200).json({
+    //     name: "Vedanti",
+    //     nickname: "Alien"
+    // });
+
+})
+
 app.listen(port, function(err){
     if(err) console.log(err);
     else console.log("Server is running on port", port);
